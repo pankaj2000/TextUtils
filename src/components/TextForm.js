07e2f0b3b38c619ded83602b1b872881847ce415
pankaj2000 <iampankaj2000@gmail.com>
2024-10-16 07:52:01 +0530

    Enchancement in textUtils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,15 +6,20 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const ConvertToLower = () => {
+    const newText = text.toLowerCase();
+    setText(newText);
+  };
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
 
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
 
   return (
-    <form>
-      <div class="mb-3">
+    <>
+      <div className="container">
         <h1 className="my-3">{props.heading}</h1>
         <label htmlFor="exampleInputEmail1" className="form-label">
           Enter your text below:
@@ -25,11 +30,23 @@ export default function TextForm(props) {
           id="floatingTextarea"
           rows={8}
           onChange={handleOnChange}
+          placeholder="Enter your text here.."
         ></textarea>
-        <buttom className="btn btn-primary my-3" onClick={ConvertToUpper}>
+        <buttom className="btn btn-secondary my-3" onClick={ConvertToUpper}>
           Convert To Upper Case
         </buttom>
+        <buttom className="btn btn-secondary my-3 mx-3" onClick={ConvertToLower}>
+          Convert To Lower Case
+        </buttom>
       </div>
-    </form>
+      <div className="container my-3">
+        <h3>Text Summery</h3>
+        <p>
+          There are {text.length} character and {text.length>0?text.split(" ").length:0} words in above text area.
+        </p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
